d5f867c5693abc15ffca2c61211a34826b35cee3
Tusharjagi <tusharjagi@gmail.com>
2024-06-02 13:54:05 +0530

    case-fusion project setup

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  important: true,
+  content: ["./src/**/*.{js,jsx,css}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
