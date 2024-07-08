cfa272ad6867a7e51e87f7d8327ae00ce7e9d85d
Tusharjagi <tusharjagi@gmail.com>
2024-07-08 22:48:27 +0530

    refactor Phone component

--- src/components/Phone.tsx
+++ src/components/Phone.tsx
@@ -1,5 +1,7 @@
-import { cn } from "../lib/utils";
 import { HTMLAttributes } from "react";
+import Image from "next/image";
+
+import { cn } from "@/lib/utils";
 
 interface PhoneProps extends HTMLAttributes<HTMLDivElement> {
   imgSrc: string;
@@ -9,14 +11,21 @@ interface PhoneProps extends HTMLAttributes<HTMLDivElement> {
 const Phone = ({ imgSrc, className, dark = false, ...props }: PhoneProps) => {
   return (
     <div className={cn("relative pointer-events-none z-50 overflow-hidden", className)} {...props}>
-      <img
+      <Image
         src={dark ? "/phone-template-dark-edges.png" : "/phone-template-white-edges.png"}
         className="pointer-events-none z-50 select-none"
         alt="phone image"
+        width={500}
+        height={500}
       />
-
       <div className="absolute -z-10 inset-0">
-        <img className="object-cover min-w-full min-h-full" src={imgSrc} alt="overlaying phone image" />
+        <Image
+          className="object-cover min-w-full min-h-full"
+          src={imgSrc}
+          alt="overlaying phone image"
+          height={500}
+          width={500}
+        />
       </div>
     </div>
   );
